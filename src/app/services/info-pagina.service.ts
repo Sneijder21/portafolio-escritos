9032b22620310dd1d1a7b5acdf69266686bd1f89
Sneijder21 <neyser36@gmail.com>
2020-10-08 11:27:10 -0500

    Fin seccion 5 utilizando firebase para nuestra automatizacion

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,14 +8,28 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  equipo: any [] = [];
   cargada = false;
   constructor( private http: HttpClient ) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
         console.log(resp);
-      });
+    });
   }
+
+  private cargarEquipo() {
+    this.http.get('https://neyser-portafolio.firebaseio.com/equipo.json')
+    .subscribe( (resp: any []) => {
+      this.equipo = resp;
+    });
+  }
+
 }
 // video 28
